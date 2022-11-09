45e43fa93d156d5284ca3844c455e9b260da6964
Foysal061 <ariful.foysal@gmail.com>
2022-11-09 23:56:48 +0100

    Employee Page structure done

---
+++ src/test/java/Pages/EmployeePage.java
@@ -0,0 +1,39 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class EmployeePage {
+    @FindBy(className = "oxd-button")
+    public List<WebElement> btnAddEmployee;
+    @FindBy(name = "firstName")
+    WebElement txtFirstName;
+    @FindBy(name = "lastName")
+    WebElement txtLastName;
+    @FindBy(css = "submit")
+    public WebElement btnSubmit;
+    @FindBy(className = "oxd-switch-input")
+    WebElement toggleButton;
+    @FindBy(className = "oxd-input")
+    List<WebElement> txtUserCreds;
+
+
+    public EmployeePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void createEmployee(String firstName, String lastName, String userName, String password, String confirmPassword) {
+        btnAddEmployee.get(2).click();
+        txtFirstName.sendKeys(firstName);
+        txtLastName.sendKeys(lastName);
+        toggleButton.click();
+        txtUserCreds.get(5).sendKeys(userName);
+        txtUserCreds.get(6).sendKeys(password);
+        txtUserCreds.get(7).sendKeys(confirmPassword);
+        btnSubmit.click();
+    }
+}
