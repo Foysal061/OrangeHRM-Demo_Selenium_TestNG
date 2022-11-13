73759965b07f73c4020fed33b02c098237652fb8
Foysal061 <ariful.foysal@gmail.com>
2022-11-13 11:52:13 +0100

    Add random Employee and save credentials to file done

--- src/test/java/testrunner/EmployeeTestRunner.java
+++ src/test/java/testrunner/EmployeeTestRunner.java
@@ -2,12 +2,22 @@ package testrunner;
 
 import Pages.EmployeePage;
 import Pages.LoginPage;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import setup.Setup;
 import utils.Utils;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
 
 import static utils.Utils.generateRandomNumber;
 
@@ -20,7 +30,7 @@ public class EmployeeTestRunner extends Setup {
     }
 
     @Test
-    public void createEmployee() {
+    public void createEmployee() throws IOException, ParseException {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList");
         EmployeePage employeePage = new EmployeePage(driver);
         Utils utils = new Utils();
@@ -28,9 +38,17 @@ public class EmployeeTestRunner extends Setup {
         String firstName = utils.getFirstname();
         String lastName = utils.getLastname();
         int randomId = generateRandomNumber(1000, 9999);
-        String userName = firstName+randomId;
+        String userName = firstName + randomId;
         String password = "P@ssword123";
         String confirmPassword = password;
         employeePage.createEmployee(firstName, lastName, userName, password, confirmPassword);
+
+        List<WebElement> headerTitle = driver.findElements(By.className("orangehrm-main-title"));
+        Assert.assertTrue(headerTitle.get(0).isDisplayed());
+
+        Utils.waitForElement(driver, headerTitle.get(0),50);
+        if (headerTitle.get(0).isDisplayed()) {
+            utils.saveJsonList(userName, password);
+        }
     }
 }
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,8 +1,21 @@
 package utils;
 
 import com.github.javafaker.Faker;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
 
 public class Utils {
     public static void scrollDown(WebDriver driver){
@@ -38,5 +51,27 @@ public class Utils {
         Faker faker = new Faker();
         setFirstname(faker.name().firstName());
         setLastname(faker.name().lastName());
+    }
+    public void saveJsonList(String username, String password) throws IOException, ParseException {
+        String fileName = "./src/test/resources/Users.json";
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(fileName));
+        JSONArray jsonArray = (JSONArray) obj;
+
+        JSONObject userObject = new JSONObject();
+        userObject.put("username", username);
+        userObject.put("password",password);
+
+        jsonArray.add(userObject);
+
+        FileWriter file = new FileWriter(fileName);
+        file.write(jsonArray.toJSONString());
+        file.flush();
+        file.close();
+        System.out.println("Saved data");
+    }
+    public static void waitForElement(WebDriver driver, WebElement element, int TIME_UNIT_SECONDS){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIME_UNIT_SECONDS));
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
